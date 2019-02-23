c803e45258d237a8f5d35332086f644e68be9d40
ghosque <yg247350078@163.com>
2019-02-23 13:28:17 +0800

    更新提示图

--- sign_up/sign_up.js
+++ sign_up/sign_up.js
@@ -2,11 +2,11 @@
 function checkOne() {
     var ACCOUNT_VALUE = document.getElementsByName("account")[0].value;
     if (ACCOUNT_VALUE == ''){
-        document.getElementById("account-img").src = "ban.jpg";
+        document.getElementById("account-img").src = "ban.png";
         document.getElementById("account-title").textContent = "请输入账号";
     }
     else {
-        document.getElementById("account-img").src = "success.jpg";
+        document.getElementById("account-img").src = "success.png";
         document.getElementById("account-title").textContent = "";
     }
 }
@@ -17,11 +17,11 @@ function checkTwo(){
 
     var PASSWORD_VALUE = document.getElementsByName("password")[0].value;
     if (PASSWORD_VALUE == ''){
-        document.getElementById("password-img").src = "ban.jpg";
+        document.getElementById("password-img").src = "ban.png";
         document.getElementById("password-title").textContent = "请输入密码";
     }
     else {
-        document.getElementById("password-img").src = "success.jpg";
+        document.getElementById("password-img").src = "success.png";
         document.getElementById("password-title").textContent = "";
     }
 }
@@ -33,17 +33,17 @@ function checkThree() {
 
     var CONFIRM_VALUE = document.getElementsByName("confirm")[0].value;
     if (CONFIRM_VALUE == ''){
-        document.getElementById("confirm-img").src = "ban.jpg";
+        document.getElementById("confirm-img").src = "ban.png";
         document.getElementById("confirm-title").textContent = "请再次输入密码";
     }
     else {
         var PASSWORD_VALUE = document.getElementsByName("password")[0].value;
         if (PASSWORD_VALUE == CONFIRM_VALUE){
-            document.getElementById("confirm-img").src = "success.jpg";
+            document.getElementById("confirm-img").src = "success.png";
             document.getElementById("confirm-title").textContent = "";
         }
         else {
-            document.getElementById("confirm-img").src = "ban.jpg";
+            document.getElementById("confirm-img").src = "ban.png";
             document.getElementById("confirm-title").textContent = "两次输入结果不一致";
         }
     }
@@ -56,11 +56,11 @@ function checkFour(){
 
     var VERIFY_VALUE = document.getElementsByName("verify")[0].value;
     if (VERIFY_VALUE == ""){
-        document.getElementById("verify-img").src = "ban.jpg";
+        document.getElementById("verify-img").src = "ban.png";
         document.getElementById("verify-title").textContent = "请输入验证码";
     }
     else {
-        document.getElementById("verify-img").src = "success.jpg";
+        document.getElementById("verify-img").src = "success.png";
         document.getElementById("verify-title").textContent = "";
     }
 }
